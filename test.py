fd15c46d8a625892268248d8063b484eff3a8cc1
Adesh Narayana Tellakula <adeshnarayana.t@outlook.com>
2026-03-17 20:05:34 +0530

    Remove API key configuration
    Removed API key configuration from test.py.

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyAQBRbMeKiyT18PLbc151QcbajZ4obu4xQ")
 model = genai.GenerativeModel("models/gemini-pro-latest")
 response = model.generate_content("Say hello")
 print(response.text)
